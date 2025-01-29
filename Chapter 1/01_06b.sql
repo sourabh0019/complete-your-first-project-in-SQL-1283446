35aa2f21ea2aa70f57b9258349b2d0a78508ed76
sourabh0019 <107043009+sourabh0019@users.noreply.github.com>
2025-01-29 14:54:59 +0000

    Task done

--- Chapter 1/01_06b.sql
+++ Chapter 1/01_06b.sql
@@ -1,1 +1,6 @@
 -- Remove null values from customer table
+SELECT * FROM Customer
+WHERE FirstName IS NULL OR
+LastName IS NULL OR Email IS NULL OR Address IS NULL
+OR City IS NULL OR State is NULL
+or Zipcode IS NULL OR Phone IS NULL;
