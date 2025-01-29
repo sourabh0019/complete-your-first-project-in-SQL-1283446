5092636c411386cb02ff8b0ffd1451b8be0bb3a2
sourabh0019 <107043009+sourabh0019@users.noreply.github.com>
2025-01-29 14:36:52 +0000

    Updated product

--- Chapter 1/01_03b.sql
+++ Chapter 1/01_03b.sql
@@ -1,1 +1,11 @@
 -- Insert new grape flavor product into table
+INSERT INTO Product(
+  Productid, Productcode,
+  ProductName, size, variety,
+  Price, Status
+)
+values (
+  17, 'MWPRA20',
+  'Mineral Water',20,
+  'Grape','1.79','ACTIVE'
+)
